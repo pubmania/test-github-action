7e7320b0b206b0a2e5152990d79524a82b1fbced
Ankit <mr.ankitmittal@gmail.com>
2024-12-13 18:29:36 +0000

    Update post_deploy.py

--- post_deploy.py
+++ post_deploy.py
@@ -80,7 +80,7 @@ def process_file_yaml(file_path, yaml_regex,access_token,at_client,image_directo
                 )
             
             response = at_client.app.bsky.feed.search_posts(params=search_params)
-            if response is not None:
+            if response.posts:
                 print("BSKY POST ALREADY EXISTS, NO ACTION NEEDED")
             else:
                 # Open the image file in binary mode
